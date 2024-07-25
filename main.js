c04f52d43edc9e0ee74756db9510b6592af96142
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-07-25 11:59:58 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+const obj = (
+    {
+        name : 'JOHN',
+        year : '30',
+        benzabak :'NEW YORK',
+        
+    }
+)
+
+// console.log(obj); 
+
+for (let i  in obj) {
+    console.log(obj[i])
+}
